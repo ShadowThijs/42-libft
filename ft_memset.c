3721dad91ea70f573101b6e4aee184f8a22a1d86
Thijs Logtenberg <tlogtenb@a2-r3-p8.s19.be>
2025-10-21 17:55:17 +0200

    ft_memset norm fix

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: tlogtenb <tlogtenb@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 21:58:01 by tlogtenb          #+#    #+#             */
-/*   Updated: 2025/08/30 21:58:01 by tlogtenb         ###   ########.fr       */
+/*   Updated: 2025/10/21 17:55:09 by tlogtenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned char *p = s;
+	unsigned char	*p;
+
+	p = s;
 	while (n--)
 		*p++ = (unsigned char)c;
-	return s;
+	return (s);
 }
